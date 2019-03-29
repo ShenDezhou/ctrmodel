b887532f8087f71306ad93818e6a6448694b58d0
ShenDezhou <bangtech@sina.com>
2019-03-29 18:03:33 +0800

    Add files via upload

--- preprocess.py
+++ preprocess.py
@@ -1,29 +1,53 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 '''
-Created on 2019��1��22��
-
+Created on 2019年1月22日
 @author: Administrator
 '''
 from PIL import Image
 import numpy
+import pandas as pd
 from builtins import int
+import pickle
 
-matrix_data = numpy.loadtxt('data/path_matrix.txt',dtype=int,delimiter='  ',ndmin=2)
-data = numpy.array(Image.open('data/blank.jpg'))
+# matrix_data = numpy.loadtxt('data/path_matrix.txt',dtype=int,delimiter='  ',ndmin=2)
+# data = numpy.array(Image.open('data/blank.jpg'))
+# 
+# print(data.shape)
+# row,col,channel=data.shape
+# for i in range(row):
+#     for j in range(col):
+#         #0-7  ~ 0-255
+#         data[i,j,0] = (matrix_data[i,j] & 0x01)* 128 +127
+#         data[i,j,1] = (matrix_data[i,j] & 0x02)* 128 +127
+#         data[i,j,2] = (matrix_data[i,j] & 0x04)* 128 +127
+#          
+# im = Image.fromarray(data)
+# im.save("image/matrix_img.jpg")
+# print('process done')
+# del matrix_data
 
-print(data.shape)
-row,col,channel=data.shape
-for i in range(row):
-    for j in range(col):
-        #0-7  ~ 0-255
-        data[i,j,0] = (matrix_data[i,j] & 0x01)* 128 +127
-        data[i,j,1] = (matrix_data[i,j] & 0x02)* 128 +127
-        data[i,j,2] = (matrix_data[i,j] & 0x04)* 128 +127
-         
-im = Image.fromarray(data)
-im.save("image/matrix_img.jpg")
-print('process done')
+wide_features=pd.read_csv('data/path_matrix.txt', sep='  ', header=None)
+deep_features=pd.read_csv('data/sns_dense.csv', sep=',', header=0)
+
+for index, row in wide_features.iterrows():
+    if not numpy.any(row):
+        row[:]=8
+        print('normalize')
+        
+maxinterval=deep_features['interval'].max()
+print(maxinterval)
+for index, row in deep_features.iterrows():
+    if row['interval'] == 0:
+        row['interval'] = maxinterval
+        print('interval normalize')
+
+
+wide_features.to_pickle('prepare/wide.pkl', compression='infer', protocol=4)
+deep_features.to_pickle('prepare/deep.pkl', compression='infer', protocol=4)
+
+print('done')
+
 # 
 # from boxoffice.image.shortestmatrix_alpha import * 
 # print(data.shape)
--- train.py
+++ train.py
@@ -2,7 +2,6 @@
 # -*- coding: utf-8 -*-
 '''
 Created on 2019年3月28日
-
 @author: Administrator
 '''
 from model import WideDeep
@@ -10,34 +9,88 @@ import pandas as pd
 from sklearn.preprocessing import LabelEncoder, MinMaxScaler
 from keras.callbacks import EarlyStopping
 from tensorflow.python.framework.dtypes import int32
-
-wide_features=pd.read_csv('data/path_matrix.txt', sep='  ', header=None)
-deep_features=pd.read_csv('data/sns_dense.csv', sep=',', header=0)
-
-le = LabelEncoder()
-deep_features.iloc[:,0]= le.fit_transform(deep_features.iloc[:,0])
-
-deep_input_feats = deep_features.iloc[:,:-1]
-deep_input = [deep_input_feats[v] for v in deep_input_feats]
-deep_input.append(wide_features.values)
-
-mms = MinMaxScaler(feature_range=(0, 10000))
-final_tags = deep_features.iloc[:,-1:].astype(int)
-print(final_tags)
-mms.fit(final_tags)
-final_tags = mms.transform(final_tags)
-print(final_tags)
+from keras import optimizers,losses
+import numpy as np
+import pickle
+from sklearn.metrics import log_loss, roc_auc_score
+import tensorflow as tf
+from builtins import int
+# from loss import auc
 
 model = WideDeep()
-model.compile(optimizer='adagrad', loss="binary_crossentropy", metrics=[],)
-
-my_callbacks = [EarlyStopping(monitor='loss', min_delta=1e-4, patience=3, verbose=1, mode='min')]
+def auc(y_true, y_pred):
+    return tf.py_func(roc_auc_score, (y_true, y_pred), tf.double)
     
+#model.compile(optimizer='rmsprop', loss=losses.mse, metrics=["mse"],)
+model.compile(optimizer="adam", loss="binary_crossentropy", metrics=[auc, 'accuracy'])
+# 
+# wide_features=pd.read_csv('data/path_matrix.txt', sep='  ', header=None)
+# deep_features=pd.read_csv('data/sns_dense.csv', sep=',', header=0)
+# 
+# for index, row in wide_features.iterrows():
+#     if not np.any(row):
+#         row[:]=8
+#         print('normalize')
+#         
+# maxinterval=deep_features['interval'].max()
+# print(maxinterval)
+# for index, row in deep_features.iterrows():
+#     if row['interval'] == 0:
+#         row['interval'] = maxinterval
+#         print('interval normalize')
+
+wide_features = pd.read_pickle("prepare/wide.pkl")
+deep_features = pd.read_pickle("prepare/deep.pkl")
+      
+print(deep_features.iloc[0, 0],deep_features.iloc[0, -1])
+deep_input_feats = deep_features.iloc[:,:-1]
+deep_input = [deep_input_feats[v] for v in deep_input_feats]
+
+# target: box value
+
+target_mms = MinMaxScaler(feature_range=(0, 1))
+final_tags = deep_features.iloc[:,[-1]]
+final_tags = target_mms.fit_transform(final_tags)
+
+for index, row in enumerate(final_tags):
+    if row > 0:
+        final_tags[index] = 1
+    else:
+        final_tags[index] = 0
+        
+deep_features=deep_features.astype({"boxoffice": int})
+print(deep_features.dtypes)
+
+print('nonzero:', np.count_nonzero(final_tags))
+
+# dense features normalization
+# http://www.faqs.org/faqs/ai-faq/neural-nets/part2/
+for i, feat in enumerate(deep_input_feats):
+    print(feat)
+    if i==0:
+        # id
+        le = LabelEncoder()
+        deep_input_feats.iloc[:,[0]]= le.fit_transform(deep_features.iloc[:,[0]])
+    else:
+        input_mms = MinMaxScaler(feature_range=(0, 1))
+        deep_input_feats.iloc[:,[i]] = input_mms.fit_transform(deep_input_feats.iloc[:,[i]])
+    
+
+deep_input.append(wide_features.values)
+
+
+my_callbacks = [EarlyStopping(monitor='loss', min_delta=1e-5, patience=3, verbose=1, mode='min')]
 model.fit(x=deep_input, y=final_tags, batch_size=64, epochs=100, verbose=1, callbacks=my_callbacks)
 
 pred = model.predict(deep_input, batch_size=2**14)
 
-pred_real = mms.inverse_transform(pred)
+pred_real = target_mms.inverse_transform(pred)
 
+s0, s1 = 0, 0
 for i, x in enumerate(pred_real):
-    print(final_tags[i], pred_real[i])
+    if pred[i]>0:
+        s1+=1
+        print(deep_features.iloc[i,0],deep_features.iloc[i,-1], final_tags[i], pred[i])
+    else:
+        s0+=1
+print('s0:',s0,'s1:',s1)
